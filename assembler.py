4203b9a27f5c794be2963ae5b754b4346189d676
David Scott <dscott764@proton.me>
2025-09-03 11:00:03 -0700

    Removes comments and whitespace.

--- assembler.py
+++ assembler.py
@@ -1,15 +1,78 @@
+"""A basic assembler for the Hack assembly language.
+
+This script takes a single command-line argument which is the path to an .asm 
+file.  It performs the first pass of the assembly process by removing all 
+comments and whitespace (including blank lines).
+
+The sanitized code is then written to a new file with the same name but with 
+an .hack extension.
+
+Usage:
+    python assembler.py YourProgram.asm
+"""
 import sys
 
 
-try:
-    filename = sys.argv[1]
+def sanitize(raw_lines):
+    """
+    Removes comments and whitespace from a list of lines.
 
-    with open(filename, 'r') as file:
-        content = file.read()
-        print(content)
-except IndexError:
-    print('Error: No file name given.')
-    print('Usage: python assembler <file name>')
-except FileNotFoundError:
-    print(f"Error: The file '{filename}' wasn't found.")
+    Args:
+        raw_lines: A list of strings.
+
+    Returns:
+        A new list of strings with comments and whitespace removed.
+    """
+    cleaned_lines  = []
+     
+    for line in raw_lines:
+        comment_start = line.find('//')
+
+        if comment_start != -1:
+            line = line[:comment_start]
+
+        processed_line = line.strip()
+
+        if processed_line:
+            cleaned_lines.append(processed_line)
+
+    return cleaned_lines
+
+
+def write_output_file(output_lines, output_file):
+    """
+    Write lines from a list to the output file.
+
+    Args:
+        output_lines: A list of lines to write.
+        output_file: A file to write the lines.
+    """
+    for line in output_lines:
+        output_file.write(line + '\n')
+
+def main():
+    """Main function to run the assembler script."""
+    try:
+        input_filename = sys.argv[1]
+
+        if input_filename.endswith('.asm'):
+            output_filename = input_filename.replace('.asm', '.hack')
+        else:
+            output_filename = input_filename + '.hack'
+
+        with open(input_filename, 'r') as infile, open(output_filename, 'w') as outfile:
+            lines_from_file = infile.readlines()
+            sanitized_code = sanitize(lines_from_file)
+            write_output_file(sanitized_code, outfile) 
+
+        print(f"Assembly successful! Output written to '{output_filename}'")
+    except IndexError:
+        print('Error: No file name given.')
+        print('Usage: python assembler <file name>')
+    except FileNotFoundError:
+        print(f"Error: The file '{sys.argv[1]}' wasn't found.")
+
+
+if __name__ == '__main__':
+    main()
 
