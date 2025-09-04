eed615a80d725c748bd1c98796cf507ec2ec29c7
David Scott <dscott764@proton.me>
2025-09-04 03:12:36 -0700

    Works on all examples without symbols.

--- assembler.py
+++ assembler.py
@@ -11,6 +11,7 @@ Usage:
     python assembler.py YourProgram.asm
 """
 import sys
+import re
 
 
 def sanitize(raw_lines):
@@ -39,6 +40,62 @@ def sanitize(raw_lines):
     return cleaned_lines
 
 
+def convert_c_instruction(c_instruction):
+    """
+    Converts a c-instruction into machine code. The binary representation of a
+    c-instruction is of the form ixxaccccccdddjjj where i is 1.
+
+    Args:
+        line: A line containing a c-instruction.
+
+    Returns:
+        A string representing a binary number.
+    """
+    # A c-instruction begins with 1. The following xx bits aren't used.
+    machine_code = '111'
+    dest = ''
+    comp = ''
+    jump = ''
+
+    if '=' in c_instruction:
+        parts = c_instruction.split('=')
+        dest = parts[0]
+        c_instruction = parts[1]
+
+    if ';' in c_instruction:
+        parts = c_instruction.split(';')
+        comp = parts[0]
+        jump = parts[1]
+    else:
+        comp = c_instruction
+
+    comp_codes = {
+        '0': '0101010', '1': '0111111', '-1': '0111010', 'D': '0001100',
+        'A': '0110000', 'M': '1110000', '!D': '0001101', '!A': '0110001',
+        '!M': '1110001', '-D': '0001111', '-A': '0110011', '-M': '1110011',
+        'D+1': '0011111', 'A+1': '0110111', 'M+1': '1110111', 'D-1': '0001110',
+        'A-1': '0110010', 'M-1': '1110010', 'D+A': '0000010', 'D+M': '1000010',
+        'D-A': '0010011', 'D-M': '1010011', 'A-D': '0000111', 'M-D': '1000111',
+        'D&A': '0000000', 'D&M': '1000000', 'D|A': '0010101', 'D|M': '1010101'
+    }
+
+    dest_codes = {
+        '': '000', 'M': '001', 'D': '010', 'MD': '011',
+        'A': '100', 'AM': '101', 'AD': '110', 'AMD': '111'
+    }
+
+    jump_codes = {
+        '': '000', 'JGT': '001', 'JEQ': '010', 'JGE': '011',
+        'JLT': '100', 'JNE': '101', 'JLE': '110', 'JMP': '111'
+    }
+
+    try:
+        machine_code += comp_codes[comp] + dest_codes[dest] + jump_codes[jump]
+        return machine_code
+    except KeyError:
+        raise ValueError(f'Invalid c-instruction: {c_instruction}')
+
+
 def convert_to_machine_code(assembler_code):
     """
     Converts assembler code into machine code.
@@ -55,7 +112,7 @@ def convert_to_machine_code(assembler_code):
         if line.startswith('@'):
             machine_code.append(format(int(line[1:]), '016b'))
         else:
-            machine_code.append(line)
+            machine_code.append(convert_c_instruction(line))
 
     return machine_code
 
